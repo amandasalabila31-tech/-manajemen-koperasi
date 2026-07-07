aadf987e28d691283f05c1a0a9267ad4c4b7606f
amandasalabila31-tech <amandasalabila31@gmail.com>
2026-07-08 01:26:43 +0700

    Add files via upload

--- koperasi/config.php
+++ koperasi/config.php
@@ -1,23 +1,8 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+echo "<pre>";
 
-$host = "ep-withered-resonance-atruyl3z-pooler.c-9.us-east-1.aws.neon.tech";
-$port = "5432";
-$dbname = "neondb";
-$user = "neondb_owner";
-$password = "npg_sL3tCwQK9dkm";
+echo "PGHOST = " . ($_ENV['PGHOST'] ?? 'tidak ada') . "\n";
+echo "PGDATABASE = " . ($_ENV['PGDATABASE'] ?? 'tidak ada') . "\n";
+echo "PGUSER = " . ($_ENV['PGUSER'] ?? 'tidak ada') . "\n";
 
-try {
-    $pdo = new PDO(
-        "pgsql:host=$host;port=$port;dbname=$dbname;sslmode=require",
-        $user,
-        $password
-    );
-
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-} catch (PDOException $e) {
-    die("Koneksi gagal: " . $e->getMessage());
-}
+exit;
