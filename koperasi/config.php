601773608df176c2c473f7c2c5ea679516420cff
amandasalabila31-tech <amandasalabila31@gmail.com>
2026-07-08 01:31:54 +0700

    Add files via upload

--- koperasi/config.php
+++ koperasi/config.php
@@ -1,8 +1,36 @@
 <?php
-echo "<pre>";
 
-echo "PGHOST = " . ($_ENV['PGHOST'] ?? 'tidak ada') . "\n";
-echo "PGDATABASE = " . ($_ENV['PGDATABASE'] ?? 'tidak ada') . "\n";
-echo "PGUSER = " . ($_ENV['PGUSER'] ?? 'tidak ada') . "\n";
+$host = "ep-withered-resonance-atruyl3z-pooler.c-9.us-east-1.aws.neon.tech";
+$port = "5432";
+$dbname = "neondb";
+$user = "neondb_owner";
+$password = "npg_sL3tCwQK9dkm";
+
+echo "<pre>";
+echo "HOST : $host\n";
+echo "DB   : $dbname\n";
+echo "USER : $user\n";
+
+try {
+    $pdo = new PDO(
+        "pgsql:host=$host;port=$port;dbname=$dbname;sslmode=require",
+        $user,
+        $password
+    );
+
+    echo "Koneksi berhasil\n\n";
+
+    $stmt = $pdo->query("
+        SELECT tablename
+        FROM pg_tables
+        WHERE schemaname='public'
+        ORDER BY tablename
+    ");
+
+    print_r($stmt->fetchAll(PDO::FETCH_COLUMN));
+
+} catch (PDOException $e) {
+    echo $e->getMessage();
+}
 
 exit;
